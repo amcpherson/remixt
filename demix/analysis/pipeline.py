8a8934cd678864815e8bd6226f6b6016121c2673
Andrew McPherson <andrew.mcpherson@gmail.com>
2015-04-06 16:39:39 -0700

    lots of extra info in output table

--- demix/analysis/pipeline.py
+++ demix/analysis/pipeline.py
@@ -163,6 +163,15 @@ def infer_cn(
 
     cn_table['log_likelihood'] = model.log_likelihood_cn(experiment.x, experiment.l, cn, h)
     cn_table['log_prior'] = model.log_prior_cn(experiment.l, cn)
+    cn_table['major_readcount'] = experiment.x[:,0]
+    cn_table['minor_readcount'] = experiment.x[:,1]
+    cn_table['total_readcount'] = experiment.x[:,2]
+    cn_table['major_expected'] = model.expected_read_count(experiment.l, cn, h, model.p)[:,0]
+    cn_table['minor_expected'] = model.expected_read_count(experiment.l, cn, h, model.p)[:,1]
+    cn_table['total_expected'] = model.expected_read_count(experiment.l, cn, h, model.p)[:,2]
+    cn_table['major_residual'] = np.absolute(cn_table['major_readcount'] - cn_table['major_expected'])
+    cn_table['minor_residual'] = np.absolute(cn_table['minor_readcount'] - cn_table['minor_expected'])
+    cn_table['total_residual'] = np.absolute(cn_table['total_readcount'] - cn_table['total_expected'])
 
     cn_table.to_csv(cn_table_filename, sep='\t', index=False, header=True)
 
