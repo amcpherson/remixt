073f87ef1eef155599e9fe7ff0825e3cb703ddb2
Andrew McPherson <andrew.mcpherson@gmail.com>
2015-11-03 21:50:06 -0600

    added breakpoint database for easy breakpoint matching

--- remixt/utils.py
+++ remixt/utils.py
@@ -4,6 +4,8 @@ import shutil
 import scipy
 import scipy.stats
 import itertools
+import collections
+import bisect
 import numpy as np
 import pandas as pd
 
@@ -111,4 +113,60 @@ def link_file(target_filename, link_filename):
     os.symlink(os.path.abspath(target_filename), link_filename)
 
 
+class BreakpointDatabase(object):
+    def __init__(self, breakpoints):
+        """ Create a database of breakpoints.
+
+        Args:
+            breakpoints (pandas.DataFrame): table of breakpoints
+
+        Breakpoints table expects the following columns:
+            'prediction_id', 'chromosome_1', 'strand_1', 'position_1',
+            'chromosome_2', 'strand_2', 'position_2'
+        """
+        self.positions = collections.defaultdict(list)
+        self.prediction_ids = collections.defaultdict(set)
+        for idx, row in breakpoints.iterrows():
+            for side in ('1', '2'):
+                self.positions[(row['chromosome_'+side], row['strand_'+side])].append(row['position_'+side])
+                self.prediction_ids[(row['chromosome_'+side], row['strand_'+side], row['position_'+side])].add((row['prediction_id'], side))
+        for key in self.positions.iterkeys():
+            self.positions[key] = sorted(self.positions[key])
+
+    def query(self, row, extend=0):
+        """ Query the database for a breakpoint.
+
+        Args:
+            row (mapping): breakpoint information
+
+        KwArgs:
+            extend (int): inexact search range
+
+        Breakpoint information expects the following keys:
+            'chromosome_1', 'strand_1', 'position_1',
+            'chromosome_2', 'strand_2', 'position_2'
+        """
+        matched_ids = list()
+        for side in ('1', '2'):
+            chrom_strand_positions = self.positions[(row['chromosome_'+side], row['strand_'+side])]
+            idx = bisect.bisect_left(chrom_strand_positions, row['position_'+side] - extend)
+            side_matched_ids = list()
+            while idx < len(chrom_strand_positions):
+                pos = chrom_strand_positions[idx]
+                dist = abs(pos - row['position_'+side])
+                if pos >= row['position_'+side] - extend and pos <= row['position_'+side] + extend:
+                    for prediction_id in self.prediction_ids[(row['chromosome_'+side], row['strand_'+side], pos)]:
+                        side_matched_ids.append((prediction_id, dist))
+                if pos > row['position_'+side] + extend:
+                    break
+                idx += 1
+            matched_ids.append(side_matched_ids)
+        matched_ids_bypos = list()
+        for matched_id_1, dist_1 in matched_ids[0]:
+            for matched_id_2, dist_2 in matched_ids[1]:
+                if matched_id_1[0] == matched_id_2[0] and matched_id_1[1] != matched_id_2[1]:
+                    matched_ids_bypos.append((dist_1 + dist_2, matched_id_1[0]))
+        if len(matched_ids_bypos) == 0:
+            return None
+        return sorted(matched_ids_bypos)[0][1]
 
