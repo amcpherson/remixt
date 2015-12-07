084f48aac5082a6b1204cfbdfb841765c1a893c4
Andrew McPherson <andrew.mcpherson@gmail.com>
2015-12-07 13:32:52 -0800

    minor: removed test code left behind from testing gradient

--- remixt/paramlearn.py
+++ remixt/paramlearn.py
@@ -35,17 +35,6 @@ def nll_negbin_partial_param(param, negbin, x, l):
     return -np.concatenate([ll_partial_g, [ll_partial_r]])
 
 
-import statsmodels.tools.numdiff
-def assert_grad_correct(func, grad, x0, *args, **kwargs):
-    """ Assert correct gradiant compared to finite difference approximation
-    """
-
-    analytic_fprime = grad(x0, *args)
-    approx_fprime = statsmodels.tools.numdiff.approx_fprime_cs(x0, func, args=args)
-
-    np.testing.assert_almost_equal(analytic_fprime, approx_fprime, 5)
-
-
 def learn_negbin_r_adjacent(negbin, x, l, min_length=10000., perc=90., bias=2.0):
     """ Learn the negative binomial dispersion parameter with the
     adjacent data points similarity assumption.
