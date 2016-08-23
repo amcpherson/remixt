006fdd9a4b42fd338b70760ec1cf8559d899b43f
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-08-23 11:30:56 -0700

    tests: improved example simulation

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -3,14 +3,21 @@ import pickle
 import numpy as np
 
 import remixt.simulations.experiment
+import remixt.cn_plot
 
 
-np.random.seed(2014)
+np.random.seed(2016)
 
 
-def generate_experiment(experiment_filename):
+def generate_experiment(experiment_filename, experiment_plot_filename):
 
-    params = {}
+    params = {
+        'N': 5000,
+        'frac_clone': [0.4, 0.2],
+        'emission_model': 'normal',
+        'num_ancestral_events': 100,
+        'num_descendent_events': 50,
+    }
 
     rh_sampler = remixt.simulations.experiment.RearrangementHistorySampler(params)
     gc_sampler = remixt.simulations.experiment.GenomeCollectionSampler(rh_sampler, params)
@@ -28,12 +35,14 @@ def generate_experiment(experiment_filename):
     with open(experiment_filename, 'w') as experiment_file:
         pickle.dump(experiment, experiment_file)
 
+    fig = remixt.cn_plot.experiment_plot(experiment, experiment.cn, experiment.h, maxcopies=6)
+    fig.savefig(experiment_plot_filename)
+
 
 if __name__ == '__main__':
 
     script_directory = os.path.realpath(os.path.dirname(__file__))
     experiment_filename = os.path.join(script_directory, 'test_experiment.pickle')
+    experiment_plot_filename = os.path.join(script_directory, 'test_experiment_plot.pdf')
 
-    generate_experiment(experiment_filename)
-
-
+    generate_experiment(experiment_filename, experiment_plot_filename)
