2db66395e098a2887d4a9586b2dd60a66e145095
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-09-11 18:33:57 -0700

    simulations: resampling rewrite

--- remixt/simulations/seqread.py
+++ remixt/simulations/seqread.py
@@ -32,7 +32,7 @@ def simulate_fragment_intervals(genome_length, num_fragments, read_length, fragm
     start = np.sort(np.random.randint(0, high=genome_length, size=num_fragments))
     length = (fragment_stddev * np.random.randn(num_fragments) + fragment_mean).astype(int)
 
-    # Filter fragments shorter than the read length 
+    # Filter fragments shorter than the read length
     is_filtered = (length < read_length) | (start + length >= genome_length)
     start = start[~is_filtered]
     length = length[~is_filtered]
@@ -125,17 +125,10 @@ def simulate_mixture_read_data(read_data_filename, genomes, read_depths, snps, p
         read_data_filename (str): file to which read data is written
         genomes (list of RearrangedGenome): rearranged genomes in mixture
         read_depths (list of float): read depths of rearranged genomes
-        snps (pandas.DataFrame): snp position data
+        snps (pandas.HDFStore): snp position data
         params (dict): dictionary of simulation parameters
 
-    Input 'snps' dataframe has columns 'chromosome', 'position', 'is_alt_0', 'is_alt_1'.
-
     """
-
-    snps = dict(list(snps.groupby('chromosome')))
-    for chromosome, chrom_snps in snps.iteritems():
-        chrom_snps.reset_index(drop=True, inplace=True)
-        chrom_snps.drop('chromosome', axis=1, inplace=True)
 
     writer = remixt.seqdataio.Writer(read_data_filename)
 
@@ -212,7 +205,7 @@ def simulate_mixture_read_data(read_data_filename, genomes, read_depths, snps, p
                 chromosome_fragment_id_start[chromosome] += len(chrom_fragments.index)
 
                 # Position data
-                chrom_snps = snps[chromosome]
+                chrom_snps = snps['/chromosome_{}'.format(chromosome)]
 
                 # Overlap snp positions and fragment intervals
                 fragment_idx, snp_idx = remixt.segalg.interval_position_overlap(
@@ -262,17 +255,10 @@ def resample_mixture_read_data(read_data_filename, source_filename, genomes, rea
         source_filename (str): file from which read data is resampled
         genomes (list of RearrangedGenome): rearranged genomes in mixture
         read_depths (list of float): read depths of rearranged genomes
-        snps (pandas.DataFrame): snp position data
+        snps (pandas.HDFStore): snp position data
         params (dict): dictionary of simulation parameters
 
-    Input 'snps' dataframe has columns 'chromosome', 'position', 'is_alt_0', 'is_alt_1'.
-
     """
-
-    snps = dict(list(snps.groupby('chromosome')))
-    for chromosome, chrom_snps in snps.iteritems():
-        chrom_snps.reset_index(drop=True, inplace=True)
-        chrom_snps.drop('chromosome', axis=1, inplace=True)
 
     segment_data = []
 
@@ -287,85 +273,91 @@ def resample_mixture_read_data(read_data_filename, source_filename, genomes, rea
     segment_data = pd.concat(segment_data, ignore_index=True)
 
     # Sum number of reads for each segment allele
-    read_data = segment_data.groupby(['chromosome', 'start', 'end', 'allele'])['num_reads'].sum().reset_index()
+    read_info = segment_data.groupby(['chromosome', 'start', 'end', 'allele'])['num_reads'].sum().reset_index()
 
     writer = remixt.seqdataio.Writer(read_data_filename)
 
-    for chromosome, chrom_read_data in read_data.groupby('chromosome'):
+    # Start of unique index for fragments, per chromosome
+    chromosome_fragment_id_start = collections.Counter()
+
+    for chromosome, chrom_read_info in read_info.groupby('chromosome'):
         source_fragments = remixt.seqdataio.read_fragment_data(source_filename, chromosome)
         source_alleles = remixt.seqdataio.read_allele_data(source_filename, chromosome)
 
         # Ensure segments are sorted by start, and the index is 0..N-1
-        chrom_segment_data = chrom_read_data[['start', 'end']].drop_duplicates().sort_values('start').reset_index(drop=True)
-        chrom_segment_data['segment_idx'] = range(len(chrom_segment_data.index))
+        chrom_segment_info = chrom_read_info[['start', 'end']].drop_duplicates().sort_values('start').reset_index(drop=True)
+        chrom_segment_info['segment_idx'] = range(len(chrom_segment_info.index))
 
         # Annotate each fragment as belonging to a single segment
         source_fragments['segment_idx'] = remixt.segalg.find_contained_segments(
-            chrom_segment_data[['start', 'end']].values, source_fragments[['start', 'end']].values)
+            chrom_segment_info[['start', 'end']].values, source_fragments[['start', 'end']].values)
 
         # Remove fragments not fully contained in segments
         source_fragments = source_fragments[source_fragments['segment_idx'] >= 0]
 
-        # SNP position data with allele information
-        chrom_snps = snps[chromosome]
-
-        # Remove homozygous positions
-        chrom_snps = chrom_snps[chrom_snps['is_alt_0'] != chrom_snps['is_alt_1']]
-
-        # Classify each read as one allele or the other
-        # SNPs table has is_alt_0, is_alt_1 for allele 0, 1
-        # source alleles table has is_alt for each snp
-        source_alleles = source_alleles.merge(chrom_snps)
-        source_alleles['allele'] = np.where(source_alleles['is_alt'] == source_alleles['is_alt_0'], 0, 1)
-
-        # Almost randomly choose an allele for each read where there is a conflict
-        source_fragment_alleles = source_alleles[['fragment_id', 'allele']].drop_duplicates('fragment_id')
-
-        # Annotate each fragment with its allele if known
-        source_fragments.set_index('fragment_id', inplace=True)
-        source_fragment_alleles.set_index('fragment_id', inplace=True)
-        source_fragments['allele'] = -1
-        source_fragments['allele'] = source_fragment_alleles['allele']
-        source_fragments.reset_index(inplace=True)
-        assert not source_fragments['allele'].isnull().any()
-
-        # Calculate proportion genotyped alleles for each segment
-        num_fragments = source_fragments.groupby('segment_idx').size()
-        num_allele_fragments = source_fragments[source_fragments['allele'] >= 0].groupby('segment_idx').size()
-        chrom_segment_data['phi'] = num_allele_fragments / num_fragments
-
-        # Annotate allele specific table with index of segment, and proportion
-        # of genotype reads
-        chrom_read_data = chrom_read_data.merge(chrom_segment_data[['segment_idx', 'start', 'end', 'phi']], on=['start', 'end'])
-        assert not chrom_read_data.isnull().any().any()
-
-        # Calculate number of reads of each type
-        chrom_read_data['num_allele_reads'] = (chrom_read_data['num_reads'] * chrom_read_data['phi']).round().astype(int)
-        chrom_read_data['num_non_allele_reads'] = (chrom_read_data['num_reads'] * (1. - chrom_read_data['phi'])).round().astype(int)
+        # Annotate chrom read info table with segment index
+        chrom_read_info = chrom_read_info.merge(chrom_segment_info)
 
         # Select fragments for each segment allele
-        for idx in chrom_read_data.index:
-            allele_fragment_ids = source_fragments.loc[
-                (source_fragments['segment_idx'] == chrom_read_data.loc[idx, 'segment_idx']) &
-                (source_fragments['allele'] == chrom_read_data.loc[idx, 'allele']),
-                ['fragment_id']]
+        sampled_fragments = []
+        for idx in chrom_read_info.index:
+            allele = chrom_read_info.loc[idx, 'allele']
+            num_reads = chrom_read_info.loc[idx, 'num_reads']
 
-            non_allele_fragment_ids = source_fragments.loc[
-                (source_fragments['segment_idx'] == chrom_read_data.loc[idx, 'segment_idx']) &
-                (source_fragments['allele'] == chrom_read_data.loc[idx, 'allele']),
-                'fragment_id'].values
+            # Sample required number of reads for this segment with replacement
+            source_segment_fragments = source_fragments[source_fragments['segment_idx'] == segment_idx]
+            sampled_segment_fragments = source_segment_fragments.loc[np.random.choice(source_segment_fragments.index, size=num_reads, replace=True)]
+            sampled_segment_fragments['allele'] = allele
 
-            # Sample required number of reads with replacement
-            sample_indices = np.random.randint(len(allele_fragment_ids.index), chrom_read_data.loc[idx, 'num_allele_reads'])
-            allele_fragment_ids = allele_fragment_ids.iloc[sample_indices]
-            sample_indices = np.random.randint(len(non_allele_fragment_ids.index), chrom_read_data.loc[idx, 'num_non_allele_reads'])
-            non_allele_fragment_ids = non_allele_fragment_ids.iloc[sample_indices]
-            sampled_fragment_ids = pd.concat([allele_fragment_ids, non_allele_fragment_ids], ignore_index=True)
+            sampled_fragments.append(sampled_segment_fragments[['start', 'end', 'allele']])
 
-            sampled_fragments = source_fragments.merge(sampled_fragment_ids)
-            sampled_alleles = source_alleles.merge(allele_fragment_ids)
+        sampled_fragments = pd.concat(sampled_fragments, ignore_index=True)
 
-            writer.write(chromosome, sampled_fragments, sampled_alleles)
+        # Reindex for subsequent index based merge
+        sampled_fragments.reset_index(drop=True, inplace=True)
+        sampled_fragments['fragment_id'] = xrange(len(sampled_fragments.index))
+        sampled_fragments['fragment_id'] += chromosome_fragment_id_start[chromosome]
+        chromosome_fragment_id_start[chromosome] += len(sampled_fragments.index)
+
+        # Position data
+        chrom_snps = snps['/chromosome_{}'.format(chromosome)]
+
+        # Overlap snp positions and fragment intervals
+        fragment_idx, snp_idx = remixt.segalg.interval_position_overlap(
+            sampled_fragments[['start', 'end']].values,
+            chrom_snps['position'].values,
+        )
+
+        # Create fragment snp table
+        fragment_snps = pd.DataFrame({'snp_idx':snp_idx, 'fragment_idx':fragment_idx})
+        fragment_snps = fragment_snps.merge(sampled_fragments, left_on='fragment_idx', right_index=True)
+        fragment_snps = fragment_snps.merge(chrom_snps, left_on='snp_idx', right_index=True)
+
+        # Keep only snps falling within reads
+        fragment_snps = fragment_snps[
+            (fragment_snps['position'] < fragment_snps['start'] + params['read_length']) |
+            (fragment_snps['position'] >= fragment_snps['end'] - params['read_length'])
+        ]
+
+        # Calculate whether the snp is the alternate based on the allele of the fragment and the genotype
+        fragment_snps['is_alt'] = np.where(
+            fragment_snps['allele'] == 0,
+            fragment_snps['is_alt_0'],
+            fragment_snps['is_alt_1'],
+        )
+
+        # Random base calling errors at snp positions
+        base_call_error = np.random.choice(
+            [True, False],
+            size=len(fragment_snps.index),
+            p=[params['base_call_error'], 1. - params['base_call_error']]
+        )
+        fragment_snps['is_alt'] = np.where(base_call_error, 1-fragment_snps['is_alt'], fragment_snps['is_alt'])
+
+        # Write out a chunk of data
+        writer.write(chromosome, sampled_fragments, fragment_snps)
+
+        num_fragments_created += len(chrom_fragments.index)
 
     writer.close()
 
