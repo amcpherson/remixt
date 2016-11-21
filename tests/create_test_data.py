e4c312eb6507a69809e2b5d3a2b5a11ba9785ef0
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-11-21 13:30:24 -0800

    simulations: minor changes to parameters of test data generation

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -15,9 +15,9 @@ def generate_experiment(experiment_filename, experiment_plot_filename):
         'N': 5000,
         'frac_normal': 0.4,
         'frac_clone': [0.4, 0.2],
-        'emission_model': 'full',
-        'num_ancestral_events': 100,
-        'num_descendent_events': 50,
+        'num_ancestral_events': 40,
+        'num_descendent_events': 20,
+        'num_false_breakpoints': 0,
     }
 
     rh_sampler = remixt.simulations.experiment.RearrangementHistorySampler(params)
