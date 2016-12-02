66420b0942152d756a56145301e28460e683c93f
dranew <andrew.mcpherson@gmail.com>
2016-12-02 12:48:27 -0800

    minor: default temp directory for mappability bwa

--- remixt/ui/mappability_bwa.py
+++ remixt/ui/mappability_bwa.py
@@ -1,5 +1,6 @@
 import argparse
 import yaml
+import os
 
 import pypeliner
 
@@ -15,6 +16,10 @@ def run(**args):
 
     pypeliner_config = config.copy()
     pypeliner_config.update(args)
+
+    if 'tmpdir' not in pypeliner_config:
+        pypeliner_config['tmpdir'] = os.path.join(ref_data_dir, 'mappability_bwa_tmp')
+
     pyp = pypeliner.app.Pypeline(config=pypeliner_config)
 
     workflow = remixt.mappability.bwa.workflow.create_bwa_mappability_workflow(config, ref_data_dir)
