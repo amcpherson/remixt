d3b301f5e9177be746cb67d0c1db298218c9e2fa
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-12-13 17:03:59 -0800

    simulations: test simulation experiment update

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -1,22 +1,24 @@
 import os
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 import remixt.simulations.experiment
 import remixt.cn_plot
+import remixt.analysis.experiment
 
 
-np.random.seed(2016)
+np.random.seed(2019)
 
 
-def generate_experiment(experiment_filename, experiment_plot_filename):
+def generate_experiment(experiment_filename, experiment_plot_filename, scatter_plot_filename):
 
     params = {
         'N': 5000,
         'frac_normal': 0.4,
-        'frac_clone_1': 0.4,
-        'num_ancestral_events': 100,
-        'num_descendent_events': 50,
+        'frac_clone_1': 0.2,
+        'num_ancestral_events': 50,
+        'num_descendent_events': 25,
         'num_false_breakpoints': 0,
         'ploidy_gaussian_mean': 2.5,
     }
@@ -40,11 +42,18 @@ def generate_experiment(experiment_filename, experiment_plot_filename):
     fig = remixt.cn_plot.experiment_plot(experiment, experiment.cn, experiment.h, maxcopies=6)
     fig.savefig(experiment_plot_filename)
 
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(111)
+    cnv = remixt.analysis.experiment.create_cn_table(experiment, experiment.cn, experiment.h)
+    remixt.cn_plot.plot_cnv_scatter(ax, cnv, major_col='major_raw', minor_col='minor_raw')
+    fig.savefig(scatter_plot_filename)
+
 
 if __name__ == '__main__':
 
     script_directory = os.path.realpath(os.path.dirname(__file__))
     experiment_filename = os.path.join(script_directory, 'test_experiment.pickle')
     experiment_plot_filename = os.path.join(script_directory, 'test_experiment_plot.pdf')
+    scatter_plot_filename = os.path.join(script_directory, 'test_scatter_plot.pdf')
 
-    generate_experiment(experiment_filename, experiment_plot_filename)
+    generate_experiment(experiment_filename, experiment_plot_filename, scatter_plot_filename)
