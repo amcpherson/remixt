3468c44e86700102bca0bd8c75e51f6be7eb158a
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-10-01 21:21:11 -0700

    build: conda dist fix

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -9,8 +9,8 @@ set -e
 #conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-echo anaconda -t $CONDA_UPLOAD_TOKEN upload conda-bld/**/$PACKAGENAME-*.tar.bz2
-anaconda -t $CONDA_UPLOAD_TOKEN upload conda-bld/**/$PACKAGENAME-*.tar.bz2
+echo anaconda -t $CONDA_UPLOAD_TOKEN upload $HOME/miniconda2/conda-bld/linux-64/$PACKAGENAME-*.tar.bz2
+anaconda -t $CONDA_UPLOAD_TOKEN upload $HOME/miniconda2/conda-bld/linux-64/$PACKAGENAME-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
