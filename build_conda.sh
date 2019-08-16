1d8633828bbdd4a724c75b7ecc001dcc6c62f86c
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-08-16 16:42:14 -0400

    build: conda upload

--- build_conda.sh
+++ build_conda.sh
@@ -1,9 +1,9 @@
 
 yum install gcc-c++ git -y
 conda config --set always_yes true
-conda config --set anaconda_upload yes
 conda config --add channels https://conda.anaconda.org/dranew
 conda config --add channels 'bioconda'
 conda install conda-build anaconda-client
 conda build conda/remixt
+anaconda -t $CONDA_UPLOAD_TOKEN upload /usr/local/conda-bld/linux-64/remixt-*.tar.bz2
 
--- run_build_conda.sh
+++ run_build_conda.sh
@@ -1,1 +1,1 @@
-docker run -v ${PWD}:/repo -w /repo -it conda/miniconda3-centos6 bash -e build_conda.sh
+docker run -v ${PWD}:/repo -w /repo -e CONDA_UPLOAD_TOKEN=$CONDA_UPLOAD_TOKEN -it conda/miniconda3-centos6 bash -e build_conda.sh
