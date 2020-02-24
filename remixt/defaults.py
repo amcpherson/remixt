486d5a71b79cd72429d50f0e823db4ef81c982af
DouglasAbrams <32776088+DouglasAbrams@users.noreply.github.com>
2020-02-24 15:34:12 -0500

    GRCh38 reference (#10)

--- remixt/defaults.py
+++ remixt/defaults.py
@@ -7,10 +7,10 @@
 ###
 
 # Version of ensembl for gene annotations
-ensembl_version                             = '70'
+ensembl_version                             = '93'
 
 # Associated genome version used by the ensembl version
-ensembl_genome_version                      = 'GRCh37'
+ensembl_genome_version                      = 'GRCh38'
 
 # Ensemble assemblies to include in the reference genome
 ensembl_assemblies                          = ['chromosome.1', 'chromosome.2', 'chromosome.3', 'chromosome.4', 'chromosome.5', 'chromosome.6', 'chromosome.7', 'chromosome.8', 'chromosome.9', 'chromosome.10', 'chromosome.11', 'chromosome.12', 'chromosome.13', 'chromosome.14', 'chromosome.15', 'chromosome.16', 'chromosome.17', 'chromosome.18', 'chromosome.19', 'chromosome.20', 'chromosome.21', 'chromosome.22', 'chromosome.X', 'chromosome.Y', 'chromosome.MT', 'nonchromosomal']
@@ -19,7 +19,7 @@ ensembl_assemblies                          = ['chromosome.1', 'chromosome.2', '
 chromosomes                                 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X']
 
 # Ensemble reference genome chromosome assemblies
-ensembl_assembly_url_template               = 'ftp://ftp.ensembl.org/pub/release-{ensembl_version}/fasta/homo_sapiens/dna/Homo_sapiens.{ensembl_genome_version}.{ensembl_version}.dna.{ensembl_assembly}.fa.gz'
+ensembl_assembly_url_template               = 'ftp://ftp.ensembl.org/pub/release-{ensembl_version}/fasta/homo_sapiens/dna/Homo_sapiens.{ensembl_genome_version}.dna.{ensembl_assembly}.fa.gz'
 
 # Ucsc genome version (must match ensembl version!)
 ucsc_genome_version                         = 'hg19'
