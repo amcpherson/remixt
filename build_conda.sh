3aa5e8c0c5a3bf76626ba390be41188c16dc7707
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-04-08 13:18:34 -0400

    minor: conda build fixes

--- build_conda.sh
+++ build_conda.sh
@@ -1,9 +1,10 @@
 
-apt-get install g++ git -y
+yum update -y
+yum group install "Development Tools" -y
 conda config --set always_yes true
 conda config --add channels https://conda.anaconda.org/dranew
 conda config --add channels 'bioconda'
 conda install conda-build anaconda-client
 conda build conda/remixt
-anaconda -t $CONDA_UPLOAD_TOKEN upload /opt/conda/conda-bld/linux-64/remixt-*.tar.bz2
+anaconda -t $CONDA_UPLOAD_TOKEN upload /usr/local/conda-bld/linux-64/remixt-*.tar.bz2
 
